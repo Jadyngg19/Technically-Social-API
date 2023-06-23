f53b1b957202401188179de415ca5c121867eee5
Jadyn Gomez <jadyngg19@gmail.com>
2023-06-23 12:16:57 -0400

    Adding in controller functions for thoughts

---
+++ Develop/Controllers/Thought-Controller.js
@@ -0,0 +1,82 @@
+const Thought = require('../Models/Thoughts-Model');
+
+const thoughtController = {
+  // Controller function to get all thoughts
+  getAllThoughts: async (req, res) => {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch thoughts.' });
+    }
+  },
+
+  // Controller function to create a new thought
+  createThought: async (req, res) => {
+    try {
+      const { thoughtText, username } = req.body;
+      const thought = await Thought.create({ thoughtText, username });
+      res.status(201).json(thought);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to create thought.' });
+    }
+  },
+
+  // Controller function to add a comment to a thought
+  addComment: async (req, res) => {
+    try {
+      const { thoughtId } = req.params;
+      const { commentBody, username } = req.body;
+
+      const thought = await Thought.findById(thoughtId);
+      if (!thought) {
+        return res.status(404).json({ error: 'Thought not found.' });
+      }
+
+      thought.comments.push({ commentBody, username });
+      await thought.save();
+      res.status(201).json(thought);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to add comment.' });
+    }
+  },
+
+// Controller function to update a thought
+updateThought: async (req, res) => {
+    try {
+      const { thoughtId } = req.params;
+      const { thoughtText } = req.body;
+
+      const thought = await Thought.findByIdAndUpdate(
+        thoughtId,
+        { thoughtText },
+        { new: true }
+      );
+      if (!thought) {
+        return res.status(404).json({ error: 'Thought not found.' });
+      }
+
+      res.json(thought);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to update thought.' });
+    }
+  },
+
+  // Controller function to delete a thought
+  deleteThought: async (req, res) => {
+    try {
+      const { thoughtId } = req.params;
+
+      const thought = await Thought.findByIdAndDelete(thoughtId);
+      if (!thought) {
+        return res.status(404).json({ error: 'Thought not found.' });
+      }
+
+      res.json({ message: 'Thought deleted successfully.' });
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to delete thought.' });
+    }
+  },
+};
+
+module.exports = thoughtController;
