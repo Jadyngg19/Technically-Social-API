30afe0e8b2d18e83ae8411e8e2825a876a93a801
Jadyn Gomez <jadyngg19@gmail.com>
2023-06-22 20:06:27 -0400

    Adding in content for server.js

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./Routes');
+const db = require('./Configuration/Connection');
+const app = express();
+const PORT = process.env.PORT || 27017;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(routes);
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
